329b49e3d06c0d2e67498c1029c473b2c637affb
sshcrack <hendrik.lind777@gmail.com>
2021-05-10 22:35:42 +0200

    Forgot to remove a = oops

--- src/cli/tools/key-checks.ts
+++ src/cli/tools/key-checks.ts
@@ -35,7 +35,7 @@ export function isWordDelete(key: RLKey) {
 }
 
 export function isTab(key: RLKey) {
-  return key.name !== "tab"
+  return key.name === "tab"
 }
 
 export function isAbort(key: RLKey) {
